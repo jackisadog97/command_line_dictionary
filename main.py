81c5902f0268bd621f8b45df0e6a4e147bdbd83b
Louis <louismacconnell@hotmail.com>
2020-02-03 15:25:10 +1030

    can find and suggestet other words using SequenceMatcher ratio method

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import json
+from difflib import SequenceMatcher
 
 data = json.load(open("data_file.json","r"))
-print(type(data))
-print(data["data"])
+
+def findWord():
+    word = input("Enter a word...\n")
+    word.lower()
+    if word in data:
+        return data[word]
+    else:
+        print("sorry word not found")
+        print("Did you mean any of these words?")
+        return similarWord(word)
+        
+
+def similarWord(word):
+    alternitives = []
+    for words in data:
+        s = SequenceMatcher(None,word,words)
+        if s.quick_ratio() > 0.75:
+            alternitives.append(words)
+    return alternitives
+
+print(findWord())
