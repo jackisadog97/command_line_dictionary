e3ad5a14aed5f31f3dc4dab8b870a0b725a0603a
Louis <louismacconnell@hotmail.com>
2020-02-04 18:49:34 +1030

    fixed issue with Proper nouns, checks before converting to lowercase

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ def findWord():
     print("Welcome to Command Line Dictionary!")
     print("Type exit() to escape")
     while True:
-        word = input("Enter a word...\n")
+        word = input("ENTER A WORD...\n")
+        if word in data:
+            for item in data[word]:
+                print(item)       #check for Proper nouns before coverting to lower
+            continue
         word = word.lower()
         if word == "exit()":
             sys.exit()
         if word in data:
-            print(data[word])
+            for item in data[word]:
+                print(item)
         else:
-            print("Could not find word")
             lst = similarWord(word)
             if len(lst) == 0:
-                 print("Sorry, no similar words found.")
+                 print("Sorry, no similar words found. Please double check it.")
             else:
+                print("Could not find word")
                 print("Enter Yes or No to the following questions")
                 for i in range(len(lst)):
                     choice = input("Did you mean %s?\n" % lst[i])
                     if 'y' in choice.lower():
-                        print(data[lst[i]])
+                        for item in data[lst[i]]:
+                            print(item)
                         break
                     elif i == len(lst) - 1:
                         print("This word doesn't seem to exist, try again!")
                     
 
 
-        
 
 def similarWord(word):
     alternitives = get_close_matches(word,data.keys(), n = 5, cutoff = 0.7)
